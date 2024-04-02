28e844a20e04bc92d7f3365ad4a30d5153da8527
Anisha Khadka <aneeshakhadka@gmail.com>
2024-04-02 17:39:38 +1100

    feature: users table created on script options

--- user_upload.php
+++ user_upload.php
@@ -2,15 +2,30 @@
 
 namespace TestCli;
 
+use Model\User;
+use TestCli\Connection\MysqlConnection;
+
+require_once __DIR__ . '/Connection/MysqlConnection.php';
+require_once __DIR__ . '/Model/User.php';
+
 $options = getopt("u:p:h:", ["file:", "create_table", "dry_run", "help"]);
 validateArguments($options);
 
+$mysql = new MysqlConnection();
+commandSwitcher($options);
+
+function commandSwitcher(array $options)
+{
+    if (isset($options["create_table"])) {
+        $user = new User();
+        $user->migrate();
+
+        echo "Table users was created sucessfully";
+    }
+}
+
 function validateArguments(array $options): void
 {
-    if (isset($options["dry_run"]) && !isset($options["file"])) {
-        die("You need to provide --file 'filepath' as well on --dry_run options");
-    }
-
     if (isset($options["file"]) && !is_file($options["file"])) {
         die("Please input valid data for options '--file'");
     }
