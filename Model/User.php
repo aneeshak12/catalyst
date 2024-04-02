0678e93252f09c3655e9933bbc2c9265a25df2f4
Anisha Khadka <aneeshakhadka@gmail.com>
2024-04-02 17:47:12 +1100

    fix: email was made unique on users table

--- Model/User.php
+++ Model/User.php
@@ -24,7 +24,7 @@ class User
                 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(30) NOT NULL,
                 surname VARCHAR(30) NOT NULL,
-                email VARCHAR(50)
+                email VARCHAR(50) UNIQUE
                 )";
 
             $this->conn->query($sql);
